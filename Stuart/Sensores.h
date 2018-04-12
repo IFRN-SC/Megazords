f9c9be89b5d43a95899ab888f90ebba73145c913
robotica@sc111481 <robotica@sc111481>
2018-04-11 21:54:15 -0300

    modifição na ordem das funcoes dos sensores

--- Stuart/Sensores.h
+++ Stuart/Sensores.h
@@ -2,30 +2,33 @@
 
 class Sensores{
 
-float valor_sensor_dir;
-float valor_sensor_mais_dir;
-float valor_sensor_esq;
-float valor_sensor_mais_esq;
-
-int mediaEsquerdo = 45;
-int mediaDireito = 45;
-int mediaMaisDireito = 45;
-int mediaMaisEsquerdo = 45;
-
-
-	public:
-
-	boolean branco_branco_branco_branco(){ return false;}
-	boolean preto_branco_branco_branco(){	return false;}
-	boolean branco_preto_branco_branco(){	return false;}
-	boolean branco_branco_preto_branco(){	return false;}
-	boolean branco_branco_branco_preto(){	return false;}
+  float valor_sensor_dir;
+  float valor_sensor_mais_dir;
+  float valor_sensor_esq;
+  float valor_sensor_mais_esq;
+  
+  int mediaEsquerdo = 45;
+  int mediaDireito = 45;
+  int mediaMaisDireito = 45;
+  int mediaMaisEsquerdo = 45;
 
   private:
-
+  
+  // Funcoes para definir o branco
+  
   boolean ehBrancoMaisEsquerdo(){return (valor_sensor_mais_esq > mediaMaisEsquerdo);}
   boolean ehBrancoEsquerdo()    {return (valor_sensor_esq > mediaEsquerdo);}
   boolean ehBrancoDireito()     {return (valor_sensor_dir > mediaDireito);}
   boolean ehBrancoMaisDireito() {return (valor_sensor_mais_dir > mediaMaisDireito);}
+
+	public:
+
+  // Funcoes para condicoes
+  
+	boolean branco_branco_branco_branco(){ return (ehBrancoMaisEsquerdo() && ehBrancoEsquerdo() && ehBrancoDireito() && ehBrancoMaisDireito());}  //BBBB
+	boolean preto_branco_branco_branco(){	 return (!ehBrancoMaisEsquerdo() && ehBrancoEsquerdo() && ehBrancoDireito() && ehBrancoMaisDireito());} //PBBB
+	boolean branco_preto_branco_branco(){	 return (ehBrancoMaisEsquerdo() && !ehBrancoEsquerdo() && ehBrancoDireito() && ehBrancoMaisDireito());} //BPBB
+	boolean branco_branco_preto_branco(){	 return (ehBrancoMaisEsquerdo() && ehBrancoEsquerdo() && !ehBrancoDireito() && ehBrancoMaisDireito());} //BBPB
+	boolean branco_branco_branco_preto(){	 return (ehBrancoMaisEsquerdo() && ehBrancoEsquerdo() && ehBrancoDireito() && !ehBrancoMaisDireito());} //BBBP
   
 };
