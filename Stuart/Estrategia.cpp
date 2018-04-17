7f10cde6b7bff87f27dd91c6686c8b3575eab1b2
robotica@sc111481 <robotica@sc111481>
2018-04-17 19:34:35 -0300

    Uso das funcoes da classe movimento na classe Estrategia

--- Stuart/Estrategia.cpp
+++ Stuart/Estrategia.cpp
@@ -1,27 +1,27 @@
 #include "Estrategia.h"
 
-#define PNEU_ANDA 45
-#define PNEU_VOLTA -45
+#define PNEU_ANDA 40
+#define PNEU_VOLTA -40
 
   void Estrategia::seguirLinha(){
 
 	   if(sensores.branco_branco_branco_branco()){            //BBBB    
-     	 robo.acionarMotores(PNEU_ANDA, PNEU_ANDA); 
+     	 movimento.seguir(); 
       
    	 }else if(sensores.preto_branco_branco_branco()){       //PBBB
-       robo.acionarMotores(PNEU_VOLTA, PNEU_ANDA);
+       movimento.girarParaEsquerda();
       
    	 }else if(sensores.branco_preto_branco_branco()){       //BPBB
-     	 robo.acionarMotores(PNEU_VOLTA, PNEU_ANDA); 
+     	 movimento.girarParaEsquerda();
       
    	 }else if(sensores.branco_branco_preto_branco()){       //BBPB
-       robo.acionarMotores(PNEU_ANDA, PNEU_VOLTA); 
+       movimento.girarParaDireita();
       
      }else if(sensores.branco_branco_branco_preto()){       //BBBP
-     	 robo.acionarMotores(PNEU_ANDA, PNEU_VOLTA); 
+     	 movimento.girarParaDireita(); 
       
      }else{
-     	 robo.acionarMotores(0,0);  	
+     	 movimento.parar();  	
      }
 	}
 	void Estrategia::desviarObstaculo(){
