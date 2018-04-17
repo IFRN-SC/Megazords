5c431d81607783ee277b5793b53c8a318d7a3d13
robotica@sc111481 <robotica@sc111481>
2018-04-17 10:10:37 -0300

    uso das funções da classe MOvimento nos retornos das funcoes de seguirLinha e adição de uma nova função

--- Stuart/Estrategia.cpp
+++ Stuart/Estrategia.cpp
@@ -3,26 +3,26 @@
 #define PNEU_ANDA 45
 #define PNEU_VOLTA -45
 
-	void Estrategia::seguirLinha(){
-    if(sensores.branco_branco_branco_branco()){            //BBBB    
-      robo.acionarMotores(PNEU_ANDA, PNEU_ANDA); 
+  void Estrategia::seguirLinha(){
+
+	   if(sensores.branco_branco_branco_branco()){            //BBBB    
+     	 robo.acionarMotores(PNEU_ANDA, PNEU_ANDA); 
       
-    }else if(sensores.preto_branco_branco_branco()){       //PBBB
-      robo.acionarMotores(PNEU_VOLTA, PNEU_ANDA);
+   	 }else if(sensores.preto_branco_branco_branco()){       //PBBB
+       robo.acionarMotores(PNEU_VOLTA, PNEU_ANDA);
       
-    }else if(sensores.branco_preto_branco_branco()){       //BPBB
-      robo.acionarMotores(PNEU_VOLTA, PNEU_ANDA); 
+   	 }else if(sensores.branco_preto_branco_branco()){       //BPBB
+     	 robo.acionarMotores(PNEU_VOLTA, PNEU_ANDA); 
       
-    }else if(sensores.branco_branco_preto_branco()){       //BBPB
-      robo.acionarMotores(PNEU_ANDA, PNEU_VOLTA); 
+   	 }else if(sensores.branco_branco_preto_branco()){       //BBPB
+       robo.acionarMotores(PNEU_ANDA, PNEU_VOLTA); 
       
-    }else if(sensores.branco_branco_branco_preto()){       //BBBP
-      robo.acionarMotores(PNEU_ANDA, PNEU_VOLTA); 
+     }else if(sensores.branco_branco_branco_preto()){       //BBBP
+     	 robo.acionarMotores(PNEU_ANDA, PNEU_VOLTA); 
       
-    }else{
-      robo.acionarMotores(0,0); 
-      
-    }
+     }else{
+     	 robo.acionarMotores(0,0);  	
+     }
 	}
 	void Estrategia::desviarObstaculo(){
 	}
