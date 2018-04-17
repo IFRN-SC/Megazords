be2003ebe8dfb1fdabc66d2ca0e684d39423ab88
robotica@sc111481 <robotica@sc111481>
2018-04-17 19:35:50 -0300

    mudança das funcoes privadas para publicas e acrescimo da funcao parar

--- Stuart/Movimento.h
+++ Stuart/Movimento.h
@@ -2,7 +2,7 @@ class Movimento{
   #define PNEU_ANDA 45
   #define PNEU_VOLTA -45
 
-	private:
+	public:
 	
 	int seguir(){
 		robo.acionarMotores(PNEU_ANDA, PNEU_ANDA);
@@ -11,5 +11,8 @@ class Movimento{
 		robo.acionarMotores(PNEU_VOLTA, PNEU_ANDA);
 	}
   int girarParaDireita(){
-    robo.acionarMotores(PNEU_ANDA, PNEU_ANDA);
-}
+    robo.acionarMotores(PNEU_ANDA, PNEU_VOLTA);
+  }int parar(){
+    robo.acionarMotores(0,0);
+  }
+};
