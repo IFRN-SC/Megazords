a5ff7c054681579fd8ec2bab1557473b3e8599da
henrique <henriqueeduardo1123@gmail.com>
2018-04-17 21:08:21 -0300

    transformei as funções para definir o branco de private para public

--- Stuart/Sensores.h
+++ Stuart/Sensores.h
@@ -1,30 +1,27 @@
 #include <robo_hardware2.h>
 
 class Sensores{
-
+  private:
+  
   float valor_sensor_dir;
   float valor_sensor_mais_dir;
   float valor_sensor_esq;
   float valor_sensor_mais_esq;
   
-  int mediaEsquerdo = 43;
-  int mediaDireito = 37;
-  int mediaMaisDireito = 47;
-  int mediaMaisEsquerdo = 45;
+  int mediaEsquerdo = 37;
+  int mediaDireito = 47;
+  int mediaMaisDireito = 45;
+  int mediaMaisEsquerdo = 43; 
 
-  private:
-  
+	public:
+
   // Funcoes para definir o branco
-  
   boolean ehBrancoMaisEsquerdo(){return (robo.lerSensorLinhaMaisEsq() > mediaMaisEsquerdo);}
   boolean ehBrancoEsquerdo()    {return (robo.lerSensorLinhaEsq() > mediaEsquerdo);}
   boolean ehBrancoDireito()     {return (robo.lerSensorLinhaDir() > mediaDireito);}
   boolean ehBrancoMaisDireito() {return (robo.lerSensorLinhaMaisDir() > mediaMaisDireito);}
 
-	public:
-
   // Funcoes para condicoes
-  
 	boolean branco_branco_branco_branco(){ return (ehBrancoMaisEsquerdo() && ehBrancoEsquerdo() && ehBrancoDireito() && ehBrancoMaisDireito());}  //BBBB
 	boolean preto_branco_branco_branco(){	 return (!ehBrancoMaisEsquerdo() && ehBrancoEsquerdo() && ehBrancoDireito() && ehBrancoMaisDireito());} //PBBB
 	boolean branco_preto_branco_branco(){	 return (ehBrancoMaisEsquerdo() && !ehBrancoEsquerdo() && ehBrancoDireito() && ehBrancoMaisDireito());} //BPBB
