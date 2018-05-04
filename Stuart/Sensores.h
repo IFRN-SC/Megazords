4fdf7856360f8fc4da885136dffad30c55019011
robotica@sc111481 <robotica@sc111481>
2018-05-03 21:48:51 -0300

    Adição do codigo do sonar

--- Stuart/Sensores.h
+++ Stuart/Sensores.h
@@ -3,14 +3,18 @@
 
 class Sensores{
 
+  private:
+  
   int mediaEsquerdo = 52;
   int mediaDireito = 44;
   int mediaMaisDireito = 47;
   int mediaMaisEsquerdo = 48;
-
-  private:
+  
+  float valor_sensor_frontal;
   
   Calibracao calibracao;
+
+  // Sensores de refletancia
   
   // Funcoes para definir o branco
   
@@ -29,5 +33,57 @@ class Sensores{
 	boolean branco_branco_preto_branco(){	 return (ehBrancoMaisEsquerdo() && ehBrancoEsquerdo() && !ehBrancoDireito() && ehBrancoMaisDireito());} //BBPB
 	boolean branco_branco_branco_preto(){	 return (ehBrancoMaisEsquerdo() && ehBrancoEsquerdo() && ehBrancoDireito() && !ehBrancoMaisDireito());} //BBBP
   boolean branco_preto_preto_branco(){   return (ehBrancoMaisEsquerdo() && !ehBrancoEsquerdo() && !ehBrancoDireito() && ehBrancoMaisDireito());}//BPPB
+
+  // Sensorares
+
+  if(robo.lerSensorSonarFrontal() <= 5 && robo.lerSensorSonarFrontal() >= 1){
+    robo.acionarMotores(0, 0);
+      delay(300);
+    robo.acionarMotores(-30, -30);
+      delay(300);
+    
+     while(robo.lerSensorLinhaEsq() > CINZAESQ){
+       robo.acionarMotores(30, -30); 
+     }
+        robo.acionarMotores(0, 0);
+          delay(300);
+          
+     while(robo.lerSensorLinhaDir() > CINZADIR){
+       robo.acionarMotores(0, -30); 
+     }
+        robo.acionarMotores(0, 0);
+          delay(300);
+          
+        robo.acionarMotores(50, 50);
+          delay(700);
+        robo.acionarMotores(0, 0);
+          delay(300);
+          
+        robo.acionarMotores(-30, 30);
+          delay(500);
+        robo.acionarMotores(0, 0);  
+          delay(300);
+          
+        robo.acionarMotores(50, 50);
+          delay(1500);
+        robo.acionarMotores(0, 0);
+          delay(300);
+          
+        robo.acionarMotores(-30, 30);
+          delay(500);
+        robo.acionarMotores(0, 0);
+          delay(300);
+
+        while(robo.lerSensorLinhaEsq() > CINZAESQ && robo.lerSensorLinhaDir() > CINZADIR){
+          robo.acionarMotores(50, 50);  
+        }
+        robo.acionarMotores(0, 0);  
+          delay(300);
+
+        while(!(robo.lerSensorLinhaEsq() > CINZAESQ && robo.lerSensorLinhaDir() > CINZADIR)){
+          robo.acionarMotores(30, -30);    
+        }
+             
+  }
   
 };
