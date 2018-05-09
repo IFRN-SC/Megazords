954d0007fec2695a872c129a808d040581a2b05d
robotica@sc111481 <robotica@sc111481>
2018-05-08 21:59:37 -0300

    Foi criada a classe Sensores.cpp

---
+++ Stuart/Sensores.cpp
@@ -0,0 +1,54 @@
+#include "Sensores.h"
+/*
+int Sensores::()
+{ 
+    if(robo.lerSensorSonarFrontal() <= 5 && robo.lerSensorSonarFrontal() >= 1){
+    robo.acionarMotores(0, 0);
+      delay(300);
+    robo.acionarMotores(-30, -30);
+      delay(300);
+    
+     while(ehBrancoEsquerdo()){
+       robo.acionarMotores(30, -30); 
+     }
+        robo.acionarMotores(0, 0);
+          delay(300);
+          
+     while(ehBrancoDireito()){
+       robo.acionarMotores(0, -30); 
+     }
+        robo.acionarMotores(0, 0);
+          delay(300);
+          
+        robo.acionarMotores(50, 50);
+          delay(700);
+        robo.acionarMotores(0, 0);
+          delay(300);
+          
+        robo.acionarMotores(-30, 30);
+          delay(500);
+        robo.acionarMotores(0, 0);  
+          delay(300);
+          
+        robo.acionarMotores(50, 50);
+          delay(1500);
+        robo.acionarMotores(0, 0);
+          delay(300);
+          
+        robo.acionarMotores(-30, 30);
+          delay(500);
+        robo.acionarMotores(0, 0);
+          delay(300);
+
+        while(ehBrancoEsquerdo() && ehBrancoDireito()){
+          robo.acionarMotores(50, 50);  
+        }
+        robo.acionarMotores(0, 0);  
+          delay(300);
+
+        while(!(ehBrancoEsquerdo() && ehBrancoDireito())){
+          robo.acionarMotores(30, -30);    
+        }       
+     }
+  }
+*/
