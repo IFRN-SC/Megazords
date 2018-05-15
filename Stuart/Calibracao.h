750038fb114636b074029a662ef1d8fcaf78c9fb
robotica@sc111485 <robotica@sc111485>
2018-05-15 15:49:09 -0300

    os dados foram movidos para o arquivo Calibracao.cpp

--- Stuart/Calibracao.h
+++ Stuart/Calibracao.h
@@ -1,19 +1,3 @@
 class Calibracao{
-
-  private:
-  /*
-  float valor_sensor_dir;
-  float valor_sensor_mais_dir;
-  float valor_sensor_esq;
-  float valor_sensor_mais_esq;
-  */
-  int mediaEsquerdo = 47;
-  int mediaDireito = 47;
-  int mediaMaisDireito = 47;
-  int mediaMaisEsquerdo = 47;
-
-  int caliPretoEsquerdo, caliPretoMaisEsquerdo, caliPretoDireito, caliPretoMaisDireito;
-  int caliBrancoEsquerdo, caliBrancoMaisEsquerdo, caliBrancoDireito, caliBrancoMaisDireito; 
-
   
 };
