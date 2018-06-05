47cc40a86f0d564553cb2c576f23c2fd629cf07b
robotica@sc111481 <robotica@sc111481>
2018-06-05 09:40:00 -0300

    Criação de um objeto calibracao

--- Stuart/Estrategia.h
+++ Stuart/Estrategia.h
@@ -1,11 +1,13 @@
 #include "Sensores.h"
 #include "Movimento.h"
+#include "Calibracao.h"
 
 class Estrategia{
 
 	private:
   Sensores sensores;
   Movimento movimento;
+  Calibracao cali;
   
 	void seguirLinha();
 	void desviarObstaculo();
