9bbeb0b3afa6312758976409417ded723519c486
robotica@sc111481 <robotica@sc111481>
2018-06-11 12:07:17 -0300

    Alteracao na visibilidade e definicao de #ifndef

--- Stuart/Calibracao.h
+++ Stuart/Calibracao.h
@@ -1,12 +1,23 @@
+#ifndef CALIBRACAO_H
+#define CALIBRACAO_H
+
 #include <robo_hardware2.h>
 
 class Calibracao{
   
   public:
-  
+  /*
   int mediaEsquerdo();
   int mediaDireito();
   int mediaMaisDireito();
-  int mediaMaisEsquerdo();
+  int mediaMaisEsquerdo();*/
   void calibrar();
+
+  protected: 
+
+  int mediaEsquerdo = 47;
+  int mediaDireito = 47;
+  int mediaMaisDireito = 47;
+  int mediaMaisEsquerdo = 47;
 };
+#endif
