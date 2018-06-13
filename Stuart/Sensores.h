bd98e7f68399bbdfe214275603750b116f824f2c
robotica@sc111481 <robotica@sc111481>
2018-06-12 21:53:01 -0300

    Mudanca de local no codigo

--- Stuart/Sensores.h
+++ Stuart/Sensores.h
@@ -1,8 +1,8 @@
 #include <robo_hardware2.h>
 #include "Calibracao.h"
 
-
 class Sensores: public Calibracao{
+  
   private:
   
   float valor_sensor_dir;
@@ -10,21 +10,21 @@ class Sensores: public Calibracao{
   float valor_sensor_esq;
   float valor_sensor_mais_esq;
   
-/*
-  int mediaEsquerdo = 50;
-  int mediaDireito = 20;
-  int mediaMaisDireito = 45;
-  int mediaMaisEsquerdo = 50; 
-*/
 	public:
 
   // Funcoes para definir o branco
-  boolean ehBrancoMaisEsquerdo(){return (robo.lerSensorLinhaMaisEsq() > mediaMaisEsquerdo);}
-  boolean ehBrancoEsquerdo()    {return (robo.lerSensorLinhaEsq() > mediaEsquerdo);}
-  boolean ehBrancoDireito()     {return (robo.lerSensorLinhaDir() > mediaDireito);}
-  boolean ehBrancoMaisDireito() {return (robo.lerSensorLinhaMaisDir() > mediaMaisDireito);}
+  
+  boolean ehBrancoMaisEsquerdo(){
+    return (robo.lerSensorLinhaMaisEsq() > mediaMaisEsquerdo);}
+  boolean ehBrancoEsquerdo()    {
+    return (robo.lerSensorLinhaEsq() > mediaEsquerdo);}
+  boolean ehBrancoDireito()     {
+    return (robo.lerSensorLinhaDir() > mediaDireito);}
+  boolean ehBrancoMaisDireito() {
+    return (robo.lerSensorLinhaMaisDir() > mediaMaisDireito);}
 
   // Funcoes para condicoes
+  
 	boolean branco_branco_branco_branco(){ return (ehBrancoMaisEsquerdo() && ehBrancoEsquerdo() && ehBrancoDireito() && ehBrancoMaisDireito());}   //BBBB
 	boolean preto_branco_branco_branco(){	 return (!ehBrancoMaisEsquerdo() && ehBrancoEsquerdo() && ehBrancoDireito() && ehBrancoMaisDireito());}  //PBBB
 	boolean branco_preto_branco_branco(){	 return (ehBrancoMaisEsquerdo() && !ehBrancoEsquerdo() && ehBrancoDireito() && ehBrancoMaisDireito());}  //BPBB
