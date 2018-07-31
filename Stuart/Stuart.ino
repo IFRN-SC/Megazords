432def5c93325f4941e060e97d186f560da9ea5b
henrique <henriqueeduardo1123@gmail.com>
2018-07-31 09:27:35 -0300

    configurei o Serial

--- Stuart/Stuart.ino
+++ Stuart/Stuart.ino
@@ -4,9 +4,11 @@
 Estrategia e;
 
 void setup() {
+  Serial.begin(9600);
   robo.configurar(false);
   e.calibracao();             
 }
 void loop() {
+  //Serial.println("Miro é o mito");
   e.executar();
 }
