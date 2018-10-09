2171a2eeac8fa2f22bdf46176574d4630102b250
henrique <henriqueeduardo1123@gmail.com>
2018-10-09 09:55:09 -0300

    foi feito os codigos para girar no verde

--- Stuart/Movimento.h
+++ Stuart/Movimento.h
@@ -21,8 +21,6 @@ class Movimento{
   void voltar(){
     robo.acionarMotores(PNEU_VOLTA, PNEU_VOLTA);
   }
-  
-
   void girarSoEsquerda(){
     robo.acionarMotores(-25, 0); 
   }
@@ -37,5 +35,46 @@ class Movimento{
   }
   void girarParaDireitaOb(){
     robo.acionarMotores(PNEU_ANDA_OBSTACULO, PNEU_VOLTA_OBSTACULO);
+  }
+  void girarVerdeEsquerdo(){
+    for(int i = 0; i < 3; i++){
+           robo.ligarLed(1);
+           delay(200);
+           robo.desligarLed(1);
+           delay(200); 
+         }
+      robo.acionarMotores(20, 20);
+      delay(300);
+      while(sensores.ehBrancoMaisEsquerdo()){
+         movimento.girarParaEsquerda(); 
+      } 
+      while(!sensores.ehBrancoMaisEsquerdo()){
+         movimento.girarParaEsquerda(); 
+      }  
+  }
+  void girarVerdeDireito(){
+    for(int i = 0; i < 3; i++){
+           robo.ligarLed(2);
+           delay(200);
+           robo.desligarLed(2);
+           delay(200); 
+         }
+      robo.acionarMotores(20, 20);
+      delay(300); 
+      while(sensores.ehBrancoMaisDireito()){
+         movimento.girarParaDireita(); 
+      }
+      while(!sensores.ehBrancoMaisDireito()){
+         movimento.girarParaDireita(); 
+      } 
+  }
+  void girarVerdeBeco(){
+    for(int i = 0; i < 3; i++){
+           robo.ligarLed(3);
+           delay(200);
+           robo.desligarLed(3);
+           delay(200); 
+         }
+      
   }
 };
