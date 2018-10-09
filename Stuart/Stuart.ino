bd862b851fca8f17805c71b58debb614aaa9f982
henrique <henriqueeduardo1123@gmail.com>
2018-10-09 09:57:03 -0300

    foi feito um codigo para ler os valores dos sensores de cor

--- Stuart/Stuart.ino
+++ Stuart/Stuart.ino
@@ -2,13 +2,18 @@
 #include "Estrategia.h"
 
 Estrategia e;
+Sensores s;
 
 void setup() {
   Serial.begin(9600);
   robo.configurar(false);
-  e.calibracao();             
+  e.calibracao();
+  robo.habilitaTCS34();             
 }
 void loop() {
   //Serial.println("Miro é o mito");
   e.executar();
+  //if (s.ehVerdeEsquerdo()) Serial.println("EH VERDE ESQUERDOOOO");
+  //Serial.println(robo.getHSVEsquerdo().v);
+  //delay(200);
 }
