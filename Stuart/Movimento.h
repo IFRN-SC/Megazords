19c0348685c6e57f3c7e1fea92b7753fe972b638
henrique <henriqueeduardo1123@gmail.com>
2018-10-29 19:36:53 -0300

    movimento
    foi organizado as funções da classe em questão.

--- Stuart/Movimento.h
+++ Stuart/Movimento.h
@@ -1,41 +1,20 @@
 class Movimento{
   #define PNEU_ANDA 40
   #define PNEU_VOLTA -40
-  #define PNEU_ANDA_OBSTACULO 40
-  #define PNEU_VOLTA_OBSTACULO -40
 
 	public:
 	
-	void seguir(){
-		robo.acionarMotores(PNEU_ANDA, PNEU_ANDA);
-	}
-	void girarParaEsquerda(){
-		robo.acionarMotores(PNEU_VOLTA, PNEU_ANDA);
-	}
-  void girarParaDireita(){
-    robo.acionarMotores(PNEU_ANDA, PNEU_VOLTA);
-  }
-  void parar(){
-    robo.acionarMotores(0,0);
-  }
-  void voltar(){
-    robo.acionarMotores(PNEU_VOLTA, PNEU_VOLTA);
-  }
+	void seguir(){robo.acionarMotores(PNEU_ANDA, PNEU_ANDA);}
+ 
+	void girarParaEsquerda(){robo.acionarMotores(PNEU_VOLTA, PNEU_ANDA);}
+ 
+  void girarParaDireita(){robo.acionarMotores(PNEU_ANDA, PNEU_VOLTA);}
   
-
-  void girarSoEsquerda(){
-    robo.acionarMotores(-25, 0); 
-  }
-  void girarSoDireita(){
-    robo.acionarMotores(0, -25); 
-  }
-  void seguirOb(){
-    robo.acionarMotores(PNEU_ANDA_OBSTACULO, PNEU_ANDA_OBSTACULO);
-  }
-  void girarParaEsquerdaOb(){
-    robo.acionarMotores(PNEU_VOLTA_OBSTACULO, PNEU_ANDA_OBSTACULO);
-  }
-  void girarParaDireitaOb(){
-    robo.acionarMotores(PNEU_ANDA_OBSTACULO, PNEU_VOLTA_OBSTACULO);
-  }
+  void parar(){robo.acionarMotores(0,0);}
+  
+  void voltar(){robo.acionarMotores(PNEU_VOLTA, PNEU_VOLTA);}
+  
+  void girarSoEsquerda(){robo.acionarMotores(-25, 0);}
+  
+  void girarSoDireita(){robo.acionarMotores(0, -25);}
 };
