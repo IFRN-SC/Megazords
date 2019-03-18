368c86252a72560b6e3fc32e80b34a596c6ec6c9
henrique <henriqueeduardo1123@gmail.com>
2019-03-18 11:22:56 -0300

    movimento:
    foi feito uma costante para cada motor, e implementadas nas funções de mivimentos.

--- Stuart/Movimento.h
+++ Stuart/Movimento.h
@@ -1,14 +1,15 @@
 class Movimento{
-  #define PNEU_ANDA 40
+  #define PNEU_ANDA_ESQ 44
+  #define PNEU_ANDA_DIR 40
   #define PNEU_VOLTA -40
 
 	public:
 	
-	void seguir(){robo.acionarMotores(PNEU_ANDA, PNEU_ANDA);}
+	void seguir(){robo.acionarMotores(PNEU_ANDA_ESQ, PNEU_ANDA_DIR);}
  
-	void girarParaEsquerda(){robo.acionarMotores(PNEU_VOLTA, PNEU_ANDA);}
+	void girarParaEsquerda(){robo.acionarMotores(PNEU_VOLTA, PNEU_ANDA_DIR);}
  
-  void girarParaDireita(){robo.acionarMotores(PNEU_ANDA, PNEU_VOLTA);}
+  void girarParaDireita(){robo.acionarMotores(PNEU_ANDA_ESQ, PNEU_VOLTA);}
   
   void parar(){robo.acionarMotores(0,0);}
   
