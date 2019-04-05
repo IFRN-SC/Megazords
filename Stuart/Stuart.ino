66747cb8d0d31dd97487f08d297b6c74e1416608
lucas <clemersonlucasocamp@gmail.com>
2019-04-05 19:52:12 -0300

    habilitamos o sensor de cor TCS34

--- Stuart/Stuart.ino
+++ Stuart/Stuart.ino
@@ -1,14 +1,19 @@
 #include <robo_hardware2.h>
 #include "Estrategia.h"
+#include "Garra.h"
 
+Garra garra1;
 Estrategia meuRobo;
 
 void setup (){
     Serial.begin(9600);
     robo.configurar(true);
+    garra1.subir ();
+    garra1.abrir ();
     robo.habilitaTCS34();
     meuRobo.calibracao();
 }
+
 
 void loop(){
     meuRobo.executar();
