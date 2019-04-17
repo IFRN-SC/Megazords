e207830c7f4ed3426ff776ac39df2011182485a8
clemersonlucas <clemersonlucasocamp@gmail.com>
2019-04-17 19:47:52 -0300

    valores das constantes alterados

--- Stuart/Garra.h
+++ Stuart/Garra.h
@@ -6,8 +6,8 @@
 class Garra {
     private :
         #define DELAY 10
-        #define ANG_MAO_FECHAR 90
-        #define ANG_MAO_ABIR 150
+        #define ANG_MAO_FECHAR 55
+        #define ANG_MAO_ABIR 5
         #define ANG_BRACO_SUBIR 120
         #define ANG_BRACO_BAIXAR 0 
         
