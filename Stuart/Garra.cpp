cad06972bfc47380c5a44219420ab10130cca980
robotica@sc111497 <robotica@sc111497>
2019-07-23 12:19:31 -0300

    GARRA
    foi ajustado alguns valores para fechar a garra.

--- Stuart/Garra.cpp
+++ Stuart/Garra.cpp
@@ -5,15 +5,15 @@ Garra::Garra(){
 }
 
 void Garra::subir (){
-    robo.acionarServoGarra2(18, 130, DELAY);
+    robo.acionarServoGarra2(28, 130, DELAY);
 }
 void Garra::baixar (){
-    robo.acionarServoGarra2(130, 18, DELAY);
+    robo.acionarServoGarra2(130, 28, DELAY);
 }
 
 void Garra::abrir (){
-    robo.acionarServoGarra1 (65, 0, DELAY);
+    robo.acionarServoGarra1 (70, 0, DELAY);
 }
 void Garra::fechar (){
-    robo.acionarServoGarra1 (0, 65, DELAY);
+    robo.acionarServoGarra1 (0, 70, DELAY);
 }
