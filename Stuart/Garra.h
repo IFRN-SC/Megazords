1b47b2c0fbe05a086745d0598c032c6d0a2584fb
Clemerson Lucas <clemersonlucascamp@gmail.com>
2019-09-01 22:21:37 -0300

    Garra.h adicionei os valores antigos as constantes

--- Stuart/Garra.h
+++ Stuart/Garra.h
@@ -6,10 +6,10 @@
 class Garra {
     private :
         #define DELAY 10
-        #define ANG_MAO_FECHAR 55
+        #define ANG_MAO_FECHAR 83
         #define ANG_MAO_ABIR 5
-        #define ANG_BRACO_SUBIR 120
-        #define ANG_BRACO_BAIXAR 0 
+        #define ANG_BRACO_SUBIR 110
+        #define ANG_BRACO_BAIXAR 7 
         
     public :
         Garra ();
