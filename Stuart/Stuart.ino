ba1111e482417efea6c579440795ad262b67fae8
robotica@sc111485 <robotica@sc111485>
2019-09-23 18:48:37 -0300

    .INO
    ajuste no ângulo da garra.

--- Stuart/Stuart.ino
+++ Stuart/Stuart.ino
@@ -8,8 +8,8 @@ Estrategia meuRobo;
 void setup (){
     Serial.begin(9600);
     robo.configurar(true);
-    robo.acionarServoGarra2(130);
-    robo.acionarServoGarra1(80);
+    robo.acionarServoGarra2(115);
+    robo.acionarServoGarra1(115);
     delay(500);
     robo.habilitaTCS34();
     meuRobo.calibracao();
