39e6a06994fae0674445685e87c000ed90c14f18
Clemerson <43820778+clemersonlucas@users.noreply.github.com>
2019-09-23 20:29:19 -0300

    Update Movimento.h
    mudei a identação do código

--- Stuart/Movimento.h
+++ Stuart/Movimento.h
@@ -1,35 +1,36 @@
 class Movimento{
-        #define PNEU_ANDA_ESQ 44
-        #define PNEU_ANDA_DIR 40
-        #define PNEU_VOLTA_DIR -40
-        #define PNEU_VOLTA_ESQ -44
+     
+     #define PNEU_ANDA_ESQ 44
+     #define PNEU_ANDA_DIR 40
+     #define PNEU_VOLTA_DIR -40
+     #define PNEU_VOLTA_ESQ -44
 
-	  public:
-        void seguir (){
-            robo.acionarMotores(PNEU_ANDA_ESQ, PNEU_ANDA_DIR);
-        }
-        
-        void voltar (){
-            robo.acionarMotores(PNEU_VOLTA_ESQ, PNEU_VOLTA_DIR);
-        }
-        
-        void parar (){
-            robo.acionarMotores(0,0);
-        }
-        
-        void girarSoEsquerda(){
-            robo.acionarMotores(-30, 0);
-        }
-        
-        void girarSoDireita (){
-            robo.acionarMotores(0, -30);
-        }
-      
-        void girarParaEsquerda (){
-            robo.acionarMotores(PNEU_VOLTA_ESQ, PNEU_ANDA_DIR);
-        }
+     public:
+	 void seguir (){
+	     robo.acionarMotores(PNEU_ANDA_ESQ, PNEU_ANDA_DIR);
+	 }
 
-        void girarParaDireita (){
-            robo.acionarMotores(PNEU_ANDA_ESQ, PNEU_VOLTA_DIR);
-        }
+	 void voltar (){
+	     robo.acionarMotores(PNEU_VOLTA_ESQ, PNEU_VOLTA_DIR);
+	 }
+
+	 void parar (){
+	     robo.acionarMotores(0,0);
+	 }
+
+	 void girarSoEsquerda(){
+	     robo.acionarMotores(-30, 0);
+	 }
+
+	 void girarSoDireita (){
+	     robo.acionarMotores(0, -30);
+	 }
+
+	 void girarParaEsquerda (){
+	     robo.acionarMotores(PNEU_VOLTA_ESQ, PNEU_ANDA_DIR);
+	 }
+
+	 void girarParaDireita (){
+	     robo.acionarMotores(PNEU_ANDA_ESQ, PNEU_VOLTA_DIR);
+	 }
 };
