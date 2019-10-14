a7034ae7341171d48cd64bcd2482bd83d1b55a12
Clemerson Lucas de Oliveira <clemersonlucasocamp@gmail.com>
2019-10-13 22:33:06 -0300

    implementei a nova função da garra no código

--- Stuart/Stuart.ino
+++ Stuart/Stuart.ino
@@ -1,16 +1,14 @@
- #include <robo_hardware2.h>
+#include <robo_hardware2.h>
 #include "Estrategia.h"
 #include "Garra.h"
 
-Garra garra1;
+Garra garra;
 Estrategia meuRobo;
 
 void setup (){
     Serial.begin(9600);
     robo.configurar(true);
-    robo.acionarServoGarra2(115);
-    robo.acionarServoGarra1(115);
-    delay(500);
+    garra.comecar ();
     robo.habilitaTCS34();
     meuRobo.calibracao();
     robo.desligarTodosLeds();
